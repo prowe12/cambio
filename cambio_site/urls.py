b9f934d06e9c2000512c7b92c5351588a446e388
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-08 08:11:41 -0800

    apply black

--- cambio_site/urls.py
+++ cambio_site/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 from cambio import views
 
 urlpatterns = [
-    path('cambio/', include('cambio.urls')),
-    path('admin/', admin.site.urls),
-    path('', views.index),
+    path("cambio/", include("cambio.urls")),
+    path("admin/", admin.site.urls),
+    path("", views.index),
 ]
