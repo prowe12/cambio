af495caffe4c86ebc944c7966cf239bfb2e0fb8d
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-08 09:58:33 -0800

    changes for fly

--- cambio_site/urls.py
+++ cambio_site/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
-# imported views
-from cambio import views
+from django.urls import path, include
 
 urlpatterns = [
-    path("cambio/", include("cambio.urls")),
     path("admin/", admin.site.urls),
-    path("", views.index),
+    path("cambio/", include("cambio.urls")),
 ]
