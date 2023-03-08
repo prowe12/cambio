1c9b735f02058a237ce52e0b3b413c8e64dda6e5
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-08 12:31:35 -0800

    Add secret key

--- cambio_site/settings.py
+++ cambio_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2#7b)08x8iz=bv#mdngg-_&b39gd1ne_&v*b75dd_t=$11!yvg"
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ["SECRET_KEY"]
+# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
