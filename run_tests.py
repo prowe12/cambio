47731deaf4e09c563a37ea1d8376888c90dd1b72
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-08 11:11:01 -0800

    Add tests back

---
+++ run_tests.py
@@ -0,0 +1,19 @@
+"""
+Run tests
+By Penny M. Rowe: 2023/01/09
+"""
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if __name__ == "__main__":
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    sys.exit(bool(failures))
