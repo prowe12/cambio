f98fb1f35af96b51a4924ca0241fcdda36ad19cb
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-18 12:23:17 -0700

    Make blue color on web page go all the way to bottom

--- cambio/templatetags/template_tags.py
+++ cambio/templatetags/template_tags.py
@@ -3,4 +3,10 @@ from django.template.defaulttags import register
 
 @register.filter
 def get_item(dictionary, key):
+    """
+    Get dictionary value from key
+    @param dictionary  The dictionary
+    @param key  The key
+    @returns  The value
+    """
     return dictionary.get(key)
